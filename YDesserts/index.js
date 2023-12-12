cbad988a4303b691018da9380715bfe6e9f66dae
Bookworm2035 <126933809+Bookworm2035@users.noreply.github.com>
2023-12-12 16:41:47 -0700

    Update index.js

--- YDesserts/index.js
+++ YDesserts/index.js
@@ -29,9 +29,9 @@ const knex = require("knex")({
    connection: {
       host:
          process.env.DB_ENDPOINT || 
-         "awseb-e-fbbnwcpagp-stack-awsebrdsdatabase-bea3yfm1iimr.cevqwie9c4vj.us-east-2.rds.amazonaws.com",
+         "awseb-e-pib2957zu6-stack-awsebrdsdatabase-epobgxcfn9yl.clg8l9gauvu4.us-east-2.rds.amazonaws.com",
    user: process.env.RDS_USERNAME || "postgres",
-   password: process.env.RDS_PASSWORD || "Comolibros44ever",
+   password: process.env.RDS_PASSWORD || "tdogtyler14",
    database: process.env.RDS_DB_NAME || "YDesserts",
    port: process.env.RDS_PORT || 5432,
    ssl: process.env.DB_SSL ? {rejectUnauthorized: false}: false 
