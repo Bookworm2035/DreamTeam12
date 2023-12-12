2bf9c3e4786981ade91bb2c7f2d137822f1c76f5
croftn <nyatcroft@gmail.com>
2023-12-12 16:16:25 -0700

    index

--- YDesserts/index.js
+++ YDesserts/index.js
@@ -32,7 +32,7 @@ const knex = require("knex")({
          "awseb-e-fbbnwcpagp-stack-awsebrdsdatabase-bea3yfm1iimr.cevqwie9c4vj.us-east-2.rds.amazonaws.com",
    user: process.env.RDS_USERNAME || "postgres",
    password: process.env.RDS_PASSWORD || "Comolibros44ever",
-   database: process.env.RDS_DB_NAME || "health_data",
+   database: process.env.RDS_DB_NAME || "YDesserts",
    port: process.env.RDS_PORT || 5432,
    ssl: process.env.DB_SSL ? {rejectUnauthorized: false}: false 
 }
@@ -60,33 +60,6 @@ app.get("/survey", (req, res) => {
    res.render("survey")
 });
 
-//Dashboard/Tableau
-app.get("/dashboard", (req, res) => {
-   res.render("dashboard");
-});
-
-app.get("/userDashboard", (req, res) => {
-   const username = req.session.username;
-   if (!username) {
-      // Redirect to the login page if username is not available
-      res.redirect("/login");
-   } else {
-      // Render the adminDashboard page with the username
-      res.render("userDashboard", { username: username });
-   }
-});
-
-app.get("/adminDashboard", (req, res) => {
-   const username = req.session.username;
-   if (!username) {
-      // Redirect to the login page if username is not available
-      res.redirect("/login");
-   } else {
-      // Render the adminDashboard page with the username
-      res.render("adminDashboard", { username: username });
-   }
-});
-
 //error page if login fails
 app.get("/error", (req, res) => {
    res.render("error");
@@ -104,19 +77,6 @@ app.get("/indexUser", (req, res) => {
    }
 });
 
-//home page for admin logged in
-app.get("/indexAdmin", (req, res) => {
-   const username = req.session.username;
-   if (!username) {
-      // Redirect to the login page if username is not available
-      res.redirect("/login");
-   } else {
-      // Render the indexUser page with the username
-      res.render("indexAdmin", { username: username });
-   }
-});
-
-
 //logout page
 app.get("/logout", (req, res) => {
    res.render("logout");
@@ -125,32 +85,24 @@ app.get("/logout", (req, res) => {
 //login page that authenticates
 app.post("/login", (req, res) => {
    const { username, password } =req.body
-   // Check if the username and password are both "admin"
-   if (username === "admin" && password === "admin") {
-      // Redirect to a different page for admin
-      req.session.username = username;
-      res.redirect("/indexAdmin");
-   } else {
-      // Check the username and password in the database
-      knex("users")
-         .where({ username, password })
-         .first()
-         .then(user => {
-            if (user) {
-               // Pass the username to the indexUser view
-               req.session.username = username;
-               // Redirect to the indexUser page
-               res.redirect("/indexUser");
-            } else {
-               // Redirect to an error page
-               res.redirect("/error");
-            }
-         })
-         .catch(err => {
-            console.error(err);
-            res.status(500).json({ error: "Internal Server Error" });
-         });
-   }
+    knex("users")
+        .where({ username, password })
+        .first()
+        .then(user => {
+        if (user) {
+            // Pass the username to the indexUser view
+            req.session.username = username;
+            // Redirect to the indexUser page
+            res.redirect("/indexUser");
+        } else {
+            // Redirect to an error page
+            res.redirect("/error");
+        }
+        })
+        .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+        });
 });
 
 
@@ -162,15 +114,6 @@ app.get("/displayUser", (req, res)=> {
    })
 })
 
-
-// Display all the users only if logged in
-app.get("/adminDisplayUser", (req, res)=> {
-   const username= req.session.username;
-    knex.select().from("users").then(users => {
-      res.render("adminDisplayUser", {myUser: users, username: username});
-   })
-})
-   
 // Displaying database
 app.get("/database", (req, res) => {
    const username= req.session.username; 
@@ -180,18 +123,6 @@ app.get("/database", (req, res) => {
       console.error('Error fetching all persons:', error);
       res.status(500).send('Error fetching all persons');
    });
-});
-app.post('/filterPersons', (req, res) => {
-   const username= req.session.username; 
-   const selectedPersonID = parseInt(req.body.PersonID); // Ensure it's a number if PersonID is numeric in the database
-   knex.select().from('persons').where('PersonID', selectedPersonID)
-      .then(filteredPersons => {
-         res.render("database", { allPersons: filteredPersons, username: username}); // Pass only filteredPersons
-      })
-      .catch(error => {
-         console.error('Error fetching filtered persons:', error);
-         res.status(500).send('Error fetching filtered persons');
-      });
 });
 
 // Site to add user to users
